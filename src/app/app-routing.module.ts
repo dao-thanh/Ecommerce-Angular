a8db18252cd677690bc6de2836c6a389dea9e963
dao-thanh <daothanh.10071999@gmail.com>
2022-11-28 09:34:21 +0700

    sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { SellerAddProductComponent } from './seller-add-product/seller-add-produ
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { SellerHomeComponent } from './seller-home/seller-home.component';
 import { SellerUpdateProductComponent } from './seller-update-product/seller-update-product.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
 
 
 
@@ -19,6 +20,7 @@ const routes: Routes = [
   { path: 'seller-update-product/:id', component: SellerUpdateProductComponent, canActivate: [AuthGuard] },
   { path: 'search/:query', component: SearchComponent },
   { path: 'details/:productId', component: ProductDetailsComponent },
+  { path: 'user-auth', component: UserAuthComponent },
 ];
 
 @NgModule({
